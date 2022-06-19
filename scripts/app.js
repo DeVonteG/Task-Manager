62b53f963cf1d584bbc8259a8ed9f7073e49bd88
DeVonte G <dagray92@hotmail.com>
2022-06-19 15:54:19 -0400

    Task Manager Competency

--- scripts/app.js
+++ scripts/app.js
@@ -50,13 +50,45 @@ function saveTask() {
     status
   );
   console.log(task);
-
-  displayTask(task);
-  $("input").val("");
-  $("select").val("");
+  /////////
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+    type: "POST",
+    data: JSON.stringify(task),
+    contentType: "application/json",
+    success: function (res) {
+      let savedTask = JSON.parse(res);
+      displayTask(task);
+      $("input").val("");
+      $("select").val("");
+      // console.log("Server Says:", res);
+    },
+    error: function (details) {
+      console.log("Error Saving", details);
+    },
+  });
+  /////////
+}
+function getStatusText(status) {
+  switch (status) {
+    case 0:
+      return "New";
+    case 1:
+      return "In Progress";
+    case 3:
+      return "Blocked";
+    case 6:
+      return "Completed";
+    case 9:
+      return "Removed";
+    default:
+      return "missing";
+  }
 }
 
 function displayTask(task) {
+  let statusText = getStatusText(task.status);
+
   let syntax = `
   <div class="tasks">
     <div class="title">
@@ -65,28 +97,82 @@ function displayTask(task) {
     <div class="locale">
       <label><b>Location:</b> ${task.location}</label>
     </div>
-    <div class="dates">
-        <label><b>Task Duration:</b> ${task.duration} Days</label>
-        <br>
-        <label><b>Due Date:</b> ${task.dueDate}</label>
+    <div class="status">
+      <label><b>Status:</b>  ${task.status}</label>
     </div>
+    <div class="dates">
+      <label><b>Task Duration:</b> ${task.duration} Days</label>
+      <br>
+      <label><b>Due Date:</b> ${task.dueDate}</label>
+    </div>
+    
   </div>`;
 
   $("#task-list").append(syntax);
 }
 
+/////////
+function testRequest() {
+  $.ajax({
+    url: "https://fsdiapi.azurewebsites.net/",
+    type: "DELETE",
+    success: function (res) {
+      console.log("Response", res);
+    },
+    error: function (errorDet) {
+      console.log("Error on request", errorDet);
+    },
+  });
+}
+
+function fetchTask() {
+  $.ajax({
+    type: "GET",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks",
+    // data: JSON.stringify(task),
+    // contentType: "application/json",
+    success: function (res) {
+      let tasks = JSON.parse(res);
+      console.log("Server Says:", tasks);
+      for (let i = 0; i < tasks.length; i++) {
+        let item = tasks[i];
+        if (item.name == "DeVonte") {
+          displayTask(item);
+        }
+      }
+    },
+    error: function (details) {
+      console.log("Error Saving", details);
+    },
+  });
+}
+
+///////////
+
+function deleteAll() {
+  $.ajax({
+    type: "DELETE",
+    url: "https://fsdiapi.azurewebsites.net/api/tasks/clear/DeVonte",
+    success: function (complete) {
+      console.log("Delete Completed", complete);
+    },
+    error: function (err) {
+      console.error(err);
+    },
+  });
+}
 function init() {
   // runTests();
 
   // console.log("task manager");
   // load data
+  fetchTask();
 
   // hook events
   $("#iImportant").click(toggleImportant);
-
   $("#btnShowHide").click(togglePanel);
-
   $("#btnSaveTask").click(saveTask);
+  $("#btnDel").click(deleteAll);
 }
 window.onload = init;
 //
